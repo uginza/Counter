cb8165fc91c5cdbd64ff5ceb3634a906bfafc592
uginza <uginza@yandex.ru>
2023-02-09 16:44:39 +0300

    Step8.Refactoring-Universal Button is done.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,33 @@
 import React, {useState} from 'react';
 import './App.module.css';
 import s from './App.module.css'
-import {ButtonInc} from "./Buttons/ButtonInc";
-import {ButtonReset} from "./Buttons/ButtonReset";
+import {Button} from "./Buttons/Button";
 import {Counter} from "./Counter/Counter";
 
 
 function App() {
+
     const [count, setCount] = useState(0);
+    const isMax = count < 5
+    const incButtonClass = `${isMax ? s.incBtn : s.incBtnDisabled}`
+    const resetButtonClass = `${count == 0 ? s.resetButtonDisabled : s.resetButton}`
+
+    function incHandler() {
+        if (isMax) {
+            setCount(count + 1)
+        }
+    }
+
+    function resetHandler() {
+        setCount(0)
+    }
 
     return (
         <div className={s.counter}>
             <Counter count={count}/>
             <div className={s.buttons}>
-                <ButtonInc setCount={setCount} value={count}/>
-                <ButtonReset setCount={setCount} value={count}/>
+                <Button onClick={incHandler} name="inc" className={incButtonClass}/>
+                <Button onClick={resetHandler} name="reset" className={resetButtonClass}/>
             </div>
 
         </div>
---
+++ src/Buttons/Button.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+type PropsType = {
+    name: string
+    className: string
+    onClick: () => void
+}
+
+
+export function Button(props: PropsType) {
+
+    return (<div>
+        <button className={props.className} onClick={props.onClick}>{props.name}</button>
+    </div>)
+}
