bd381627d3c5b16a14c750b1628810211d39a54c
uginza <uginza@yandex.ru>
2023-04-01 11:00:30 +0300

    -Styles corrected with flex 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,18 @@
-import React, {useState} from 'react';
+import React from 'react';
+import CounterInterface from "./CounterInterface/CounterInterface";
+import SetIntervalComponent from "./SetIntervalConponent/SetIntervalComponent";
 import s from './App.module.css'
-import {Button} from "./Buttons/Button";
-import {Counter} from "./Counter/Counter";
-
+import k from '../src/CounterInterface/CounterInterface.module.css'
+import c from '../src/SetIntervalConponent/SetIntervalComponent.module.css'
 
 function App() {
 
-    const [count, setCount] = useState(0);
-    const isMax = count < 5
-    const incButtonClass = `${isMax ? s.incBtn : s.incBtnDisabled}`
-    const resetButtonClass = `${count == 0 ? s.resetButtonDisabled : s.resetButton}`
-
-    function incHandler() {
-        if (isMax) {
-            setCount(count + 1)
-        }
-    }
-
-    function resetHandler() {
-        setCount(0)
-    }
-
     return (
         <div className={s.counterContainer}>
-            <div className={s.counter}>
-               <div className={s.integerContainer}>
-                   <Counter count={count}/>
-               </div>
-
-                <div className={s.buttons}>
-                    <Button onClick={incHandler} name="inc" className={incButtonClass}/>
-                    <Button onClick={resetHandler} name="reset" className={resetButtonClass}/>
-                </div>
-            </div>
+            <div className={c.SetIntervalContainer}><SetIntervalComponent/></div>
+            <div className={k.counterInterfaceContainer}><CounterInterface/></div>
         </div>
-    );
+    )
 }
 
 export default App;
---
+++ src/SetIntervalConponent/SetIntervalComponent.tsx
@@ -0,0 +1,41 @@
+import React, {useState} from 'react';
+import s from './SetIntervalComponent.module.css'
+import {Button} from "../Buttons/Button";
+import {Counter} from "../Counter/Counter";
+
+
+
+function SetIntervalComponent() {
+
+    const [count, setCount] = useState(0);
+    const isMax = count < 5
+    const incButtonClass = `${isMax ? s.incBtn : s.incBtnDisabled}`
+    const resetButtonClass = `${count == 0 ? s.resetButtonDisabled : s.resetButton}`
+
+    function incHandler() {
+        if (isMax) {
+            setCount(count + 1)
+        }
+    }
+
+    function resetHandler() {
+        setCount(0)
+    }
+
+    return (
+        <div className={s.counterContainer}>
+            <div className={s.counter}>
+               <div className={s.integerContainer}>
+                   <Counter count={count}/>
+               </div>
+
+                <div className={s.buttonsContainer}>
+                    <Button onClick={incHandler} name="inc" className={incButtonClass}/>
+                    <Button onClick={resetHandler} name="reset" className={resetButtonClass}/>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default SetIntervalComponent;
