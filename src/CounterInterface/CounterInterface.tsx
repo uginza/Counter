ab71d048a4971e164c66e800892c3d7ee231b1a2
uginza <uginza@yandex.ru>
2023-04-04 14:29:56 +0300

    integerClass1 for CounterInterface.tsx is set

--- src/CounterInterface/CounterInterface.tsx
+++ src/CounterInterface/CounterInterface.tsx
@@ -16,11 +16,10 @@ function CounterInterface(props:CounterInterfaceType) {
     const [count, setCount] = useState(0);
     const isMin=props.newMinValue
     useEffect(()=>setCount(isMin),[isMin])
-
     const isMax =!props.newMaxValue?count<5: count < props.newMaxValue
     const incButtonClass = `${isMax ? i.incBtn : i.incBtnDisabled}`
     const resetButtonClass = `${count === 0 ? i.resetButtonDisabled : i.resetButton}`
-    const integerClass1 = `${count < props.newMaxValue? s.integerContainerActive :s.integerContainerDisabled}`
+    const integerClass1 = `${isMax? s.integerContainerActive :s.integerContainerDisabled }`
 
     function incHandler() {
         if (isMax) {
